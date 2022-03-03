d6084a5c55e0fa1f6fe844549bcb70be82aa7ed1
Anishgir <anishgir6263@gmail.com>
2022-03-03 11:29:59 +0530

    made changes

--- javascript/swiggy.js
+++ javascript/swiggy.js
@@ -1,125 +1,153 @@
-const body = document.body;
+const pdpModule = (function() {
+    function fetchCategories() {
+        return [{
+                "displayName": "Recommended",
+                "id": "recommended"
+            },
+            {
+                "displayName": "Dessert and Beverages",
+                "id": "dessert_beverage"
+            },
+            {
+                "displayName": "Biryani",
+                "id": "biryani"
+            }
+        ];
+    };
 
-const categories = [{
-		"displayName": "Recommended",
-		"id": "recommended"
-	},
-	{
-		"displayName": "Dessert and Beverages",
-		"id": "dessert_beverage"
-	},
-	{
-		"displayName": "Biryani",
-		"id": "biryani"
-	}
-];
+    const categories = fetchCategories();
 
-const menuItems = [{
-		"id": "2121",
-		"displayName": "Kadhai Paneer Biryani",
-		"price": 249,
-		"currency": "INR",
-		"vegetarian": true,
-		"imgUrl": "https://media.istockphoto.com/photos/fish-biryani-with-basmati-rice-indian-food-picture-id488481490?k=20&m=488481490&s=612x612&w=0&h=HYP2KxiC1e2tAtzmfrA7xxs3u8LD1wjSLPUD9bZ48eU=",
-		"categories": ["recommended", "biryani"]
-	},
-	{
-		"id": "2122",
-		"displayName": "Real Biryani",
-		"price": 349,
-		"currency": "INR",
-		"vegetarian": false,
-		"imgUrl": "https://media.istockphoto.com/photos/hyderabadi-biryani-a-popular-chicken-or-mutton-rice-preparation-picture-id466089615?k=20&m=466089615&s=612x612&w=0&h=_Z0Jlombq-VX8Pl8I9mJf_kIuvbzZ7j8ucxvRoLL8BM=",
-		"categories": ["biryani"]
-	},
-	{
-		"id": "2123",
-		"displayName": "Plain Veg Biryani",
-		"price": 149,
-		"currency": "INR",
-		"vegetarian": true,
-		"imgUrl": "https://media.istockphoto.com/photos/chicken-biryani-directly-above-photo-picture-id1169141170?k=20&m=1169141170&s=612x612&w=0&h=EpCF3lQF2GBRaVApNELuE5xFQfv8fyQ_wWC52hmyxeo=",
-		"categories": ["biryani"]
-	}
-];
+    function fetchMenuItems() {
+        return [{
+                "id": "2121",
+                "displayName": "Kadhai Paneer Biryani",
+                "price": 249,
+                "currency": "INR",
+                "vegetarian": true,
+                "imgUrl": "https://media.istockphoto.com/photos/fish-biryani-with-basmati-rice-indian-food-picture-id488481490?k=20&m=488481490&s=612x612&w=0&h=HYP2KxiC1e2tAtzmfrA7xxs3u8LD1wjSLPUD9bZ48eU=",
+                "categories": ["recommended", "biryani"]
+            },
+            {
+                "id": "2122",
+                "displayName": "Real Biryani",
+                "price": 349,
+                "currency": "INR",
+                "vegetarian": false,
+                "imgUrl": "https://media.istockphoto.com/photos/hyderabadi-biryani-a-popular-chicken-or-mutton-rice-preparation-picture-id466089615?k=20&m=466089615&s=612x612&w=0&h=_Z0Jlombq-VX8Pl8I9mJf_kIuvbzZ7j8ucxvRoLL8BM=",
+                "categories": ["biryani"]
+            },
+            {
+                "id": "2123",
+                "displayName": "Plain Veg Biryani",
+                "price": 149,
+                "currency": "INR",
+                "vegetarian": true,
+                "imgUrl": "https://media.istockphoto.com/photos/chicken-biryani-directly-above-photo-picture-id1169141170?k=20&m=1169141170&s=612x612&w=0&h=EpCF3lQF2GBRaVApNELuE5xFQfv8fyQ_wWC52hmyxeo=",
+                "categories": ["biryani"]
+            }
+        ];
+    }
 
-const cartItems = {
-	"lineItems": [{
-		"id": "1121",
-		"name": "Plain Veg Biryani",
-		"quantity": 2,
-		"price": 149,
-		"currency": "INR"
-	}],
-	"shippingFee": 0,
-	"discount": 0,
-	"tax": 0,
-	"subTotal": 149
-}
+    const menuItems = fetchMenuItems();
 
+    function fetchCartItems() {
+        return {
+            "lineItems": [{
+                "id": "1121",
+                "name": "Plain Veg Biryani",
+                "quantity": 2,
+                "price": 149,
+                "currency": "INR"
+            }],
+            "shippingFee": 0,
+            "discount": 0,
+            "tax": 0,
+            "subTotal": 149
+        }
+    }
 
-const mp = new Map();
-menuItems.forEach(element => {
-	element.categories.forEach(key => {
-		if (!mp.has(key)) {
-			mp.set(key, []);
-		}
-		mp.get(key).push(element);
-	});
-});
+    const cartItems = fetchCartItems();
 
-const main = document.querySelector("main");
+    const itemsByCategoryMap = new Map();
+    menuItems.forEach(element => {
+        element.categories.forEach(key => {
+            if (!itemsByCategoryMap.has(key)) {
+                itemsByCategoryMap.set(key, []);
+            }
+            itemsByCategoryMap.get(key).push(element);
+        });
+    });
 
-const divCategories = document.querySelector(".categories");
+    const main = document.querySelector("main");
 
-const ulCategories = document.createElement("ul");
-categories.forEach(element => {
-	const liCategories = document.createElement("li");
-	liCategories.id = element.id;
-	liCategories.innerText = element.displayName;
-	ulCategories.append(liCategories);
-});
-divCategories.append(ulCategories);
+    const categoriesContainer = document.querySelector(".categories");
 
-const divMenuItems = document.querySelector(".menu-items");
+    const categoriesList = document.createElement("ul");
+    categories.forEach(element => {
+        const categoryName = document.createElement("li");
+        categoryName.id = element.id;
+        categoryName.innerText = element.displayName;
+        categoriesList.append(categoryName);
+    });
+    categoriesContainer.append(categoriesList);
 
-for (const [key, value] of mp.entries()) {
-	const divTop = document.createElement("div");
-	divTop.className = "top";
-	divTop.innerHTML = `<h3>${key}</h3>
-                      <p>${mp.get(key).length} Items</p>`
-	divMenuItems.append(divTop);
+    const menuItemsContainer = document.querySelector(".menu-items");
 
-	for (let i = 0; i < mp.get(key).length; i++) {
-		const divDiscription = document.createElement("div");
-		divDiscription.className = "itemDiscription";
-		divDiscription.innerHTML = `<div>
-                                  <img src="images/veg_mark.png" alt="veg mark logo" class="vegMark"> 
-                                  <p class="dishname">${mp.get(key)[i].displayName}</p>
-                                  <p> &#8377 ${mp.get(key)[i].price}</p>
-                                </div>
-                                <div class="container">
-                                <img src=${mp.get(key)[i].imgUrl} alt="food Image" class=foodImage>
-                                <button class="addItem">ADD</button>
-                                </div>`;
+    function addCategoryInfo(key) {
+        return `<h3>${key}</h3>
+		<p>${itemsByCategoryMap.get(key).length} Items</p>`
+    }
 
-		divMenuItems.append(divDiscription);
-	}
-}
+    function addItemDescription(key, index) {
+        return `<div>
+		<img src="images/veg_mark.png" alt="Veg Mark Logo" class="veg-mark">
+		<p class="dish-name">${itemsByCategoryMap.get(key)[index].displayName}</p>
+		<p> &#8377 ${itemsByCategoryMap.get(key)[index].price}</p>
+		</div>
+		<div class="container">
+		<img src=${itemsByCategoryMap.get(key)[index].imgUrl} alt="food Image" class=item-image>
+		<button class="add-item-btn">ADD</button>
+		</div>`;
+    }
 
-const divCart = document.querySelector(".cart");
+    function addItemToCartegory(key, index) {
+        const itemContainer = document.createElement("div");
+        itemContainer.className = "item-description";
+        itemContainer.innerHTML = addItemDescription(key, index);
 
-divCart.innerHTML = `<div class = "cartDescription">
-                        <h3>Cart</h3>
-                        <p>${cartItems.lineItems.length} Items</p>
-                        <br>
-                        <p class="dishname">${cartItems.lineItems[0].name}</p>
-                        <span>
-                            <p>Subtotal</p>
-                            <p> &#8377 ${cartItems.subTotal}</p>
-                        </span>
-                        <p id="extracharges"><small>Extra charges may apply</small></p>
-                        <button id="checkout_btn">Checkout &#8594 </button>
-                      </div>`
+        menuItemsContainer.append(itemContainer);
+    }
 
-main.append(divCategories, divMenuItems, divCart);
+    function addCategory(key) {
+        const categoryInfoContainer = document.createElement("div");
+        categoryInfoContainer.className = "category-info";
+        categoryInfoContainer.innerHTML = addCategoryInfo(key);
+        menuItemsContainer.append(categoryInfoContainer);
+        for (let i = 0; i < itemsByCategoryMap.get(key).length; i++) {
+            addItemToCartegory(key, i);
+        }
+    }
+
+    for (const [key, value] of itemsByCategoryMap.entries()) {
+        addCategory(key);
+    }
+
+    const cartContainer = document.querySelector(".cart");
+
+    function addCartDescription() {
+        return `<div class = "cart-description">
+				<h3>Cart</h3>
+				<p>${cartItems.lineItems.length} Items</p>
+				<br>
+				<p class="dish-name">${cartItems.lineItems[0].name}</p>
+				<span>
+					<p>Subtotal</p>
+					<p> &#8377 ${cartItems.subTotal}</p>
+				</span>
+				<p class="charges-description"><small>Extra charges may apply</small></p>
+				<button class="checkout-btn">Checkout &#8594 </button>
+				</div>`;
+    }
+    cartContainer.innerHTML = addCartDescription();
+    main.append(categoriesContainer, menuItemsContainer, cartContainer);
+})();
