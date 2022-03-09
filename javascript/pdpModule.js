9d3fd9e9ef2a87ed8b93b670bff74986abc00633
Anishgir <anishgir6263@gmail.com>
2022-03-09 10:06:26 +0530

    mvc first commit

--- javascript/pdpModule.js
+++ javascript/pdpModule.js
@@ -108,76 +108,120 @@ function getItem(itemList, index) {
 }
 
 const pdpModule = (function() {
-    
-    function Model(){
+    return {
+        init: display,
+    };
+
+    function Model() {
         this.categories = fetchCategories();
         this.menuItems = fetchMenuItems();
         this.cartItems = fetchCartItems();
-        this.itemsByCategoryMap = createItemByCategoryMap(menuItems);
+        this.itemsByCategoryMap = createItemByCategoryMap(this.categories, this.menuItems);
+        this.bindOnSelectingCategoryChanged = (callback) => {
+            this.onSelectingCategory = callback;
+        }
+        this.onSelectingCategoryChangeMenuList = (categoryName) => {
+            this.list = [{
+                "displayName": categoryName
+            }];
+            this.itemsByCategoryMap = createItemByCategoryMap(this.list, this.menuItems);
+            this.onSelectingCategory(this.list, this.cartItems, this.itemsByCategoryMap);
+        }
     }
 
-    // return {
-    //     init: display,
-    // };
+    function View() {
+        this.createView = (categories, cartItems, itemsByCategoryMap) => {
+            createCategoriesView(categories);
+            createMenuItemsView(itemsByCategoryMap);
+            createCartView(cartItems);
+        }
+        this.bindOnSelectingCategory = (handler) => {
+            document.querySelectorAll(".categories ul li").forEach((listItem) => listItem.addEventListener("click", (e) => {
+                e.target.style.color = '#FFA500';
+                handler(e.target.innerText);
 
-
-    function View(){
-        const main = document.querySelector("main");
-        addCategoryToMain(main);
-        addMenuListToMain(main,itemsByCategoryMap);
-        addCartToMain(main);
+                document.querySelectorAll('.categories ul li').forEach(element => {
+                    if (e.target.id != element.id)
+                        element.style.color = '#291D38';
+                })
+            }));
+        }
     }
 
-    function Controller(model,view){
+    function Controller(model, view) {
         this.model = model;
         this.view = view;
+        this.view.createView(this.model.categories, this.model.cartItems, this.model.itemsByCategoryMap);
+        this.onSelectingCategory = (categories, cartItems, itemsByCategoryMap) => {
+            this.view.createView(categories, cartItems, itemsByCategoryMap);
+        }
+        this.handelOnSelectingCategory = (categoryName) => {
+            this.model.onSelectingCategoryChangeMenuList(categoryName);
+        }
+        this.view.bindOnSelectingCategory(this.handelOnSelectingCategory);
+        this.model.bindOnSelectingCategoryChanged(this.onSelectingCategory);
     }
 
     function display() {
-        
+        const app = new Controller(new Model(), new View());
     }
 
     function addItemByCategoryInMap(map, key, menuItem) {
-        if (!map.has(key)) {
-            map.set(key, []);
+        if (map.has(key)) {
+            map.get(key).push(menuItem);
         }
-        map.get(key).push(menuItem);
     }
 
     function addMenuItemsInMap(map, menuItem) {
         menuItem.categories.forEach(key => addItemByCategoryInMap(map, key, menuItem));
     }
 
-    function addEmptyListByKeyInMap(map,key) {
+    function addEmptyListByKeyInMap(map, key) {
         if (!map.has(key.displayName.toLowerCase())) {
             map.set(key.displayName.toLowerCase(), []);
         }
     }
 
-    function createItemByCategoryMap(menuItems) {
+    function createItemByCategoryMap(categories, menuItems) {
         const map = new Map();
-        categories.forEach(key => addEmptyListByKeyInMap(map,key));
+        categories.forEach(key => addEmptyListByKeyInMap(map, key));
         menuItems.forEach(menuItem => addMenuItemsInMap(map, menuItem));
         return map;
     }
 
+    function isIncluded(listElement) {
+        let flag = false;
+        document.querySelectorAll(".categories ul li").forEach(item => {
+            if (item.innerText === listElement.displayName)
+                flag = true;
+        });
+        return flag;
+    }
+
     function addListItemInCategoryList(categoriesList, listElement) {
+        if (isIncluded(listElement))
+            return;
+
         const categoriesListElement = document.createElement("li");
         categoriesListElement.id = listElement.id;
         categoriesListElement.innerText = listElement.displayName;
         categoriesList.append(categoriesListElement);
     }
 
-    function addCategoryToMain(main) {
+    function createCategoriesView(categories) {
+        const main = document.querySelector("main");
         const categoriesContainer = document.querySelector(".categories");
         const categoriesList = document.createElement("ul");
         categories.forEach(listElement => addListItemInCategoryList(categoriesList, listElement));
-        categoriesContainer.append(categoriesList);
 
-        main.append(categoriesContainer);
+        const numberOfListElement = categoriesList.getElementsByTagName("li").length;
+        if (numberOfListElement) {
+            categoriesContainer.append(categoriesList);
+            main.append(categoriesContainer);
+        }
     }
 
-    function addItemToCartegory(menuItemsContainer, itemsByCategoryMap, key, index) {
+    function addItemsToCategoryInMenuList(menuItemsContainer, itemsByCategoryMap, key, index) {
         const itemContainer = document.createElement("div");
         itemContainer.className = "item-description";
         itemContainer.innerHTML = createItemTemplate(itemsByCategoryMap, key, index);
@@ -185,29 +229,32 @@ const pdpModule = (function() {
         menuItemsContainer.append(itemContainer);
     }
 
-    function addMenuItemsToCategory(menuItemsContainer, itemsByCategoryMap, key) {
+    function addCategoriesToMenuList(menuItemsContainer, itemsByCategoryMap, key) {
         const categoryInfoContainerElement = document.createElement("div");
         categoryInfoContainerElement.className = "category-info";
         categoryInfoContainerElement.innerHTML = createCategoryInfoTemplate(itemsByCategoryMap, key);
         menuItemsContainer.append(categoryInfoContainerElement);
         for (let i = 0; i < itemsByCategoryMap.get(key).length; i++) {
-            addItemToCartegory(menuItemsContainer, itemsByCategoryMap, key, i);
+            addItemsToCategoryInMenuList(menuItemsContainer, itemsByCategoryMap, key, i);
         }
     }
 
-    function addCategoriesToMenuList(menuItemsContainer, itemsByCategoryMap) {
+    function createMenuList(menuItemsContainer, itemsByCategoryMap) {
         for (const [key] of itemsByCategoryMap.entries()) {
-            addMenuItemsToCategory(menuItemsContainer, itemsByCategoryMap, key);
+            addCategoriesToMenuList(menuItemsContainer, itemsByCategoryMap, key);
         }
     };
 
-    function addMenuListToMain(main,itemsByCategoryMap) {
+    function createMenuItemsView(itemsByCategoryMap) {
+        const main = document.querySelector("main");
         const menuItemsContainer = document.querySelector(".menu-items");
-        addCategoriesToMenuList(menuItemsContainer, itemsByCategoryMap);
+        menuItemsContainer.innerHTML = "";
+        createMenuList(menuItemsContainer, itemsByCategoryMap);
         main.append(menuItemsContainer);
     }
 
-    function addCartToMain(main) {
+    function createCartView(cartItems) {
+        const main = document.querySelector("main");
         const cartContainer = document.querySelector(".cart");
         cartContainer.innerHTML = createCartDescriptionTemplate(cartItems);
         main.append(cartContainer);
